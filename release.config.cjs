fd895c6b8bde6eec0ed5925ed2f9ef6d9f96b795
Mark Martirosian <hello@markmartirosian.com>
2021-10-11 22:52:29 +0400

    fix: release

--- release.config.cjs
+++ release.config.cjs
@@ -1,6 +1,5 @@
 const { readdirSync } = require('fs')
 const path = require('path')
-const GITHUB_REPOSITORY = process.env.GITHUB_REPOSITORY
 
 const assets = ['RPMS', 'SRPMS']
   .flatMap((folder) => readdirSync(path.join(__dirname, folder))
@@ -14,32 +13,11 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    process.env['RELEASE_CONTAINER'] === 'true' ? [
-      '@semantic-release/exec',
-      {
-        shell: true,
-        publishCmd: [
-          [
-            'docker buildx build',
-            '--cache-from type=local,src=$HOME/.buildx',
-            '--cache-to   type=local,dest=$HOME/.buildx,mode=max',
-            '--platform linux/amd64,linux/arm64/v8',
-            '--tag ghcr.io/GITHUB_REPOSITORY:latest',
-            '--tag ghcr.io/GITHUB_REPOSITORY:${nextRelease.version}',
-            '--push .'
-          ].join(' ')
-        ]
-          .map((string) =>
-            string.replace(/GITHUB_REPOSITORY/g, GITHUB_REPOSITORY)
-          )
-          .join(' && \\\n')
-      }
-    ] : undefined,
-    process.env['RELEASE_ASSETS'] === 'true' ? [
+    [
       '@semantic-release/github',
       {
         assets
       }
-    ] : undefined,
-  ].filter((value) => value !== undefined)
+    ]
+  ]
 }
